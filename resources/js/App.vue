8647805208ff18d645df73344991c1988d925fb8
aymansohag <aymansohag.contact@gmail.com>
2023-01-28 00:12:38 +0600

    vue setup

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Vue Setup</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import App from './App.vue'
+
+createApp(App).mount('#app');
