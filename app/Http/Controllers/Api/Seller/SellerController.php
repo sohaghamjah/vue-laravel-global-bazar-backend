bab74f35e37ed115911d2d69b42d99ed578e6b11
aymansohag <aymansohag.contact@gmail.com>
2023-12-15 21:24:06 +0600

    seller filtering

--- app/Http/Controllers/Api/Seller/SellerController.php
+++ app/Http/Controllers/Api/Seller/SellerController.php
@@ -15,10 +15,19 @@ class SellerController extends Controller
         return SellerResource::collection($sellers);
     }
 
-    public function sellerProducts($slug){
+    public function sellerProducts($slug, Request $request){
         try {
             $seller = Seller::with('products')->where('slug',$slug)->first();
-            return new SellerResource($seller);
+
+            $sort = $request->sort;
+
+            if($sort == 'default'){
+                $products = $seller->products()->paginate($request->show);
+            }else{
+                $products = $seller->products()->conditions($sort)->paginate($request->show);
+            }
+            return (new SellerResource($seller))
+                    ->additional(['products' => $products]);
         } catch (\Throwable $e) {
             return sendMessage($e->getMessage(), false, 500);
         }
