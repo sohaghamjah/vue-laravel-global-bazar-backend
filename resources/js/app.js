837a02affec8927851d77617f07c77736c81b399
aymansohag <aymansohag.contact@gmail.com>
2024-03-07 00:27:43 +0600

    admin lte setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
-import './bootstrap';
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js";
+import "admin-lte/dist/js/adminlte.min.js";
 
 import { createApp } from 'vue';
 import App from './App.vue'
--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -3,12 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css','resources/js/app.js'])
     </head>
-    <body>
-        <div id="app"></div>
-        @vite('resources/js/app.js')
+    <body class="hold-transition sidebar-mini layout-fixed">
+        <div class="wrapper" id="app"></div>
     </body>
 </html>
