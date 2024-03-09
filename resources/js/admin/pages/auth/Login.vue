301c51d41254b1223230b053537b734284351470
aymansohag <aymansohag.contact@gmail.com>
2024-03-09 10:00:31 +0600

    input component

--- resources/js/admin/pages/auth/Login.vue
+++ resources/js/admin/pages/auth/Login.vue
@@ -1,18 +1,24 @@
 <script setup>
-    
+    import Input from '@/common/components/Input.vue';
+    import { reactive } from 'vue';
+
+    const form = reactive({
+      full_name: "",
+      password: "",
+    })
 </script>
 <template>
     <!-- /.login-logo -->
   <div class="card card-outline card-primary">
     <div class="card-header text-center">
-      <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+      <a href="../../index2.html" class="h1">Admin Login</a>
     </div>
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
       <form action="../../index3.html" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <Input placeholder="Full Name" type="email" v-model="form.full_name"></Input>
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -20,7 +26,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <Input placeholder="Password" type="password" v-model="form.password"></Input>
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -44,22 +50,6 @@
         </div>
       </form>
 
-      <div class="social-auth-links text-center mt-2 mb-3">
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
-      <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
     </div>
     <!-- /.card-body -->
   </div>
---
+++ resources/js/common/components/Input.vue
@@ -0,0 +1,9 @@
+<script setup>
+      defineProps(['modelValue']);
+      defineEmits(['update:modelValue']);
+</script>
+<template>
+      <input class="form-control" 
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)">    
+</template>
