403cd476ee24c909cb4d3c7ff52a9ad3e0230766
aymansohag <aymansohag.contact@gmail.com>
2024-03-12 00:06:47 +0600

    reusable modal create

--- resources/js/common/components/modal/Basic.vue
+++ resources/js/common/components/modal/Basic.vue
@@ -4,18 +4,20 @@
     }
 </script>
 <template>
-        <!-- Modal -->
-        <div class="modal fade" id="modal_wrapper">
+    <!-- Modal -->
+    <div class="modal fade" id="modal_wrapper" tabindex="-1">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLabel">
+                    <slot name="title" />
+                </h5>
                 <button type="button" @click.prevent="closeModal" class="close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
+            <slot />
             </div>
             <div class="modal-footer">
                 <button type="button" @click.prevent="closeModal" class="btn btn-secondary">Close</button>
