34d5701d1e4274b7337c3a5bd0e99460a76d7da9
aymansohag <aymansohag.contact@gmail.com>
2024-03-12 22:55:40 +0600

    file manager migration file

---
+++ app/Models/FileManager.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FileManager extends Model
+{
+    use HasFactory;
+}
